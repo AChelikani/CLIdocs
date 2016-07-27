3d2be8f39aad3337990953f20fc169c8640e4d29
Advith Chelikani <advith.chelikani@gmail.com>
2016-07-27 15:36:48 -0700

    Update api.py

--- api.py
+++ api.py
@@ -2,9 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return '{"Keyword" : "print", "ArgList" : "words"}'
+@app.route('/python/<path:command>')
+def index(command):
+    if command == "print":
+        return '{"Name": "print", "DataType": "function", "Declaration": "print [string]", "Description": "To print something", "ExampleUsage": "print"}'
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
