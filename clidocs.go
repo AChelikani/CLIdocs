e38a3c9212946a0c4fd0767336bdc1a7d2852c8d
Advith Chelikani <advith.chelikani@gmail.com>
2016-07-27 15:36:27 -0700

    Update clidocs.go

--- clidocs.go
+++ clidocs.go
@@ -4,17 +4,32 @@ import (
   "fmt"
   "os"
   "encoding/json"
+  //"reflect"
 
   "github.com/urfave/cli"
 )
 
 type Response struct {
-  Keyword string
-  ArgList string
+  Name string
+  DataType string
+  Declaration string
+  Description string
+  ExampleUsage string
+}
+
+// Given a Response object, cleanly prints it to the command line
+func cleanPrint(r Response) {
+  fmt.Printf("\n %s, %s, %s\n", r.Name, r.DataType, r.Declaration)
+  fmt.Printf("\n %s\n", r.Description)
+  fmt.Printf("\n %s\n", r.ExampleUsage)
+  fmt.Printf("\n")
 }
 
 func main() {
   app := cli.NewApp()
+  app.Name = "CLIdocs"
+  app.Usage = "Command line documentation for your favorite languages!"
+  app.Version = "1.0.0"
 
   app.Flags = []cli.Flag {
     cli.StringFlag{
@@ -25,21 +40,19 @@ func main() {
   }
 
   app.Action = func(c *cli.Context) error {
-    //query := "print"
+    query := "print"
     if c.NArg() > 0 {
-      //query = c.Args().Get(0)
+      query = c.Args().Get(0)
     }
-    if c.String("lang") == "python" {
-      var res Response
-      b := getJSON("")
-      err := json.Unmarshal(b, &res)
-      if (err != nil) {
-        // deal with error
-      }
-      fmt.Println(res)
-    } else {
-      //fmt.Println("Hello", name)
+    var url string = c.String("lang") + "/" + query
+    var res Response
+    content := getJSON(url)
+    err := json.Unmarshal(content, &res)
+    if (err != nil) {
+      // deal with error
     }
+    //fmt.Printf("%+v\n ", res)
+    cleanPrint(res)
     return nil
   }
 
